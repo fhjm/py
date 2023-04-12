8abf909f6fd708a8506668eed8f58a8f96eb0cda
Neil108 <87611191+fhjm@users.noreply.github.com>
2023-04-12 22:56:27 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,5 +1,19 @@
-print("安安")
-B=float(input("你Der身高(公分)："))
-C=float(input("你Der體重:)(好我知道不能亂問體重但我還是要知道)："))
-BMI=A/(B/100*B/100)
-print("你的BMI是{BMI}")
+height = float(input("請輸入身高（單位：公分）："))
+weight = float(input("請輸入體重（單位：公斤）："))
+
+# 計算BMI
+bmi = weight / ((height/100) ** 2)
+
+# 根據BMI值顯示不同的結果
+if bmi < 18.5:
+    print("您的BMI值為", bmi, "屬於體重過輕。")
+elif 18.5 <= bmi < 24:
+    print("您的BMI值為", bmi, "屬於正常範圍。")
+elif 24 <= bmi < 27:
+    print("您的BMI值為", bmi, "稍微有些過重。")
+elif 27 <= bmi < 30:
+    print("您的BMI值為", bmi, "屬於輕度肥胖。")
+elif 30 <= bmi < 35:
+    print("您的BMI值為", bmi, "屬於中度肥胖。")
+else:
+    print("您的BMI值為", bmi, "屬於重度肥胖。")
